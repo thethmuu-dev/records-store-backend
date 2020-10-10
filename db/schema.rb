adaf616e529d6625fc12af1aae54b085dbc4d55f
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2020-10-10 21:42:11 +0630

    Installed Vue Front-End

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_155600) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_artists_on_user_id"
